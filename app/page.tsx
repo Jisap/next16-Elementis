1138f75ecc8d080f45b03864e9df419e97de8ad1
Jorge Matos <jisapjisap@gmail.com>
2026-08-14 15:08:21 +0200

    feat: add Introduction section and integrate it into the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Client/Navbar";
 import Hero from "@/sections/Hero/Index";
+import Introduction from "@/sections/Introduction";
 
 const Home = () => {
   return (
     <main className="min-h-[200vh]">
       <Navbar />
       <Hero />
+      <Introduction />
     </main>
   );
 };
---
+++ sections/Introduction/Server.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IntroductionServer = () => {
+    return (
+        <div>IntroductionServer</div>
+    )
+}
+
+export default IntroductionServer
