7a7619876856e500598e17868c855df49fb576e5
Jorge Matos <jisapjisap@gmail.com>
2026-08-17 12:19:56 +0200

    feat: implement landing page layout and add reusable Label component with supporting asset

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Innovation from "@/components/Client/Innovation";
 import Navbar from "@/components/Client/Navbar";
 import ElementisStory from "@/sections/ElementisStory";
+import Form from "@/sections/Form";
 import Hero from "@/sections/Hero/Index";
 import Introduction from "@/sections/Introduction";
 import SustainableRetreat from "@/sections/SustainableRetreat";
@@ -16,7 +17,7 @@ const Home = () => {
       <Innovation />
       <ElementisStory />
       <SustainableRetreat />
-      {/* <Form /> */}
+      <Form />
     </main>
   );
 };
